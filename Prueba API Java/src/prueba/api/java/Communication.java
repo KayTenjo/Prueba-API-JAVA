a8bb7b56a12d7c2db99a2eb780ba7dfb09f3cf94
Rodrigo Monsalve <kayj.tune@gmail.com>
2015-12-26 18:52:11 -0300

    Librerías de comunicación listas, sin probar

--- Prueba API Java/src/prueba/api/java/Communication.java
+++ Prueba API Java/src/prueba/api/java/Communication.java
@@ -5,10 +5,65 @@
  */
 package prueba.api.java;
 
+import com.fazecast.jSerialComm.*;
+import java.util.Scanner;
+
 /**
  *
  * @author kayjt
  */
 public class Communication {
     
+    SerialPort port;
+    Scanner scanner;
+    
+    public Communication()
+    {
+    }
+    public Communication(String portName, int baudRate)  
+    {
+        port = SerialPort.getCommPort(portName);
+        port.setBaudRate(baudRate);
+        port.openPort();
+        scanner = new Scanner(port.getInputStream());
+    }
+
+    public String[] getPortNames() {
+
+        String [] portNames = new String [SerialPort.getCommPorts().length];
+        for (int i=0; i < SerialPort.getCommPorts().length; i++){
+        
+            portNames[i] = SerialPort.getCommPorts()[i].getSystemPortName();
+            
+        }
+        return portNames;      
+    }
+
+    public void openPort(String portName, int baudRate)
+    {
+        scanner.close();
+        port.closePort();
+        port = SerialPort.getCommPort(portName);
+        port.setBaudRate(baudRate);
+        port.openPort();
+    }
+
+    public void write(String data)
+    {
+        byte[] buffer = data.getBytes();
+        port.writeBytes(buffer, buffer.length);
+    }
+
+    public String ReadLine()
+    {
+        return scanner.nextLine();
+        
+    }
+
+    public void ClosePort()
+    {
+        scanner.close();
+        port.closePort();
+    }
+   
 }
